41cd715195d703f7f94dcac8588064595e65c1e1
Tenghuan He <tenghuanhe@gmail.com>
2022-03-10 11:49:29 +0800

    cpplint

--- attach_agent.c
+++ attach_agent.c
@@ -8,25 +8,25 @@ JNIEXPORT jint JNICALL Agent_OnAttach(JavaVM* jvm, char* options,
   jvmtiEnv* jvmti;
   jvmtiError error;
   jint res;
-  int i;
+  jint class_count;
   jclass* classes;
-  jint count = 0;
+  int i;
   char* signature;
 
-  res = (*jvm)->GetEnv(jvm, (void**)&jvmti, JVMTI_VERSION_1_0);
+  res = (*jvm)->GetEnv(jvm, (void **) &jvmti, JVMTI_VERSION_1_0);
   if (res != JNI_OK || jvmti == NULL) {
-    printf("Unable to get access to JVMTI verson 1.0");
+    printf("Unable to get access to JVMTI verson 1.0\n");
   }
 
-  error = (*jvmti)->GetLoadedClasses(jvmti, &count, &classes);
+  error = (*jvmti)->GetLoadedClasses(jvmti, &class_count, &classes);
   if (error) {
     printf("ERROR: JVMTI GetLoadedClasses failed!\n");
   }
 
-  for (i = 0; i < count; i++) {
+  for (i = 0; i < class_count; i++) {
     (*jvmti)->GetClassSignature(jvmti, classes[i], &signature, NULL);
     printf("class signature=%s\n", signature);
   }
 
-  return error;
+  return JNI_OK;
 }
--- load_agent.c
+++ load_agent.c
@@ -4,14 +4,14 @@
 #include "jvmti.h"
 
 static void check_jvmti_errors(jvmtiEnv* jvmti, jvmtiError errnum,
-                               const char* str) {
+                               const char* errmsg) {
   if (errnum != JVMTI_ERROR_NONE) {
     char* errnum_str;
     errnum_str = NULL;
     (void)(*jvmti)->GetErrorName(jvmti, errnum, &errnum_str);
-    printf("ERROR: JVMTI: [%d] %s - %s", errnum,
+    printf("ERROR: JVMTI: [%d] %s - %s\n", errnum,
            (errnum_str == NULL ? "Unknown" : errnum_str),
-           (str == NULL ? "" : str));
+           (errmsg == NULL ? "" : errmsg));
   }
 }
 
@@ -20,46 +20,45 @@ static void JNICALL callbackMethodEntry(jvmtiEnv* jvmti, JNIEnv* jni,
   char* name_ptr;
   char* signature_ptr;
   char* generic_ptr;
+  jvmtiError err;
 
-  jvmtiError error;
-  error = (*jvmti)->GetMethodName(jvmti, method, &name_ptr, &signature_ptr,
-                                  &generic_ptr);
-  check_jvmti_errors(jvmti, error, "Unable to get the method name");
+  err = (*jvmti)->GetMethodName(jvmti, method, &name_ptr, &signature_ptr,
+                                &generic_ptr);
+  check_jvmti_errors(jvmti, err, "Unable to get the method name.");
   printf("Entered method %s %s\n", signature_ptr, name_ptr);
 }
 
 JNIEXPORT jint JNICALL Agent_OnLoad(JavaVM* jvm, char* options,
                                     void* reserved) {
   jvmtiEnv* jvmti;
-  jvmtiError error;
+  jvmtiError err;
   jint res;
   jvmtiEventCallbacks callbacks;
   jvmtiCapabilities capa;
   jrawMonitorID rawMonitorID;
 
-  res = (*jvm)->GetEnv(jvm, (void**)&jvmti, JVMTI_VERSION_1_0);
+  res = (*jvm)->GetEnv(jvm, (void **)&jvmti, JVMTI_VERSION_1_0);
+
   if (res != JNI_OK || jvmti == NULL) {
     printf("Unable to get access to JVMTI verson 1.0.\n");
   }
 
   (void)memset(&capa, 0, sizeof(jvmtiCapabilities));
   capa.can_generate_method_entry_events = 1;
-  error = (*jvmti)->AddCapabilities(jvmti, &capa);
-  check_jvmti_errors(jvmti, error, "Unable to add the required capabilites.");
+  err = (*jvmti)->AddCapabilities(jvmti, &capa);
+  check_jvmti_errors(jvmti, err, "Unable to add the required capabilites.");
 
-  error = (*jvmti)->SetEventNotificationMode(
+  err = (*jvmti)->SetEventNotificationMode(
       jvmti, JVMTI_ENABLE, JVMTI_EVENT_METHOD_ENTRY, (jthread)NULL);
-  check_jvmti_errors(jvmti, error,
-                     "Unable to set the event notification mode.");
+  check_jvmti_errors(jvmti, err, "Unable to set the event notification mode.");
 
   (void)memset(&callbacks, 0, sizeof(callbacks));
   callbacks.MethodEntry = &callbackMethodEntry;
-  error =
-      (*jvmti)->SetEventCallbacks(jvmti, &callbacks, (jint)sizeof(callbacks));
-  check_jvmti_errors(jvmti, error, "Unable to set event callbacks.");
+  err = (*jvmti)->SetEventCallbacks(jvmti, &callbacks, (jint)sizeof(callbacks));
+  check_jvmti_errors(jvmti, err, "Unable to set event callbacks.");
 
-  error = (*jvmti)->CreateRawMonitor(jvmti, "JVMTI agent data", &rawMonitorID);
-  check_jvmti_errors(jvmti, error, "Unable to create a Raw monitor.");
+  err = (*jvmti)->CreateRawMonitor(jvmti, "JVMTI agent data", &rawMonitorID);
+  check_jvmti_errors(jvmti, err, "Unable to create a Raw monitor.");
 
   return JNI_OK;
 }
